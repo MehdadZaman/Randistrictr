14e357aa67444ecc3037b40abdf4528777baa8d2
Lawrence <lawlin@MacBook-Air.local>
2021-09-25 22:31:48 -0400

    Remove unused component and add highlighting to map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useMemo } from 'react';
-import { MapContainer, TileLayer, ZoomControl, GeoJSON } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  ZoomControl,
+  GeoJSON,
+  useMapEvents,
+} from 'react-leaflet';
 import { center, zoom, bounds } from '../constants/map';
 // Components
 import Navbar from './navbar';
 import Sidebar from './sidebar';
 import Position from './Position';
-import M from './M';
 // JSON
 import statesdata from '../json/states.json';
 import MaryLandCongressionalDistricts from '../json/congressional-districts/maryland_congressional_districts.json';
@@ -50,79 +55,90 @@ const Map = () => {
   const [map, setMap] = useState(null);
   const [selectedState, setSelectedState] = useState();
 
-  // const displayMap = useMemo(() => {
-  //   const highlightFeature = (e) => {
-  //     let layer = e.target;
-  //     // const { NAME_2, NAME_3, COUNT } = e.target.feature.properties;
-  //     // setSelected({
-  //     //   province: `${NAME_3}, ${NAME_2}`,
-  //     //   count: COUNT
-  //     // });
-  //     layer.setStyle({
-  //       weight: 2,
-  //       color: '#DF1995',
-  //       dashArray: '',
-  //       fillOpacity: 1,
-  //     });
-  //     // if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-  //     //   layer.bringToFront();
-  //     // }
-  //   };
-  //   const resetHighlight = (e) => {
-  //     // setSelected({});
-  //     e.target.setStyle(style(e.target.feature));
-  //   };
-  //   const zoomToFeature = (e) => {
-  //     const bounds = e.target.getBounds();
-  //     map.fitBounds([bounds.getSouthWest(), bounds.getNorthEast()]);
-  //   };
+  const displayMap = useMemo(() => {
+    let mapRef;
+    function MapEvents() {
+      const map = useMapEvents({
+        zoomend: () => {
+          onZoomEnd(map);
+        },
+        locationfound: (location) => {
+          console.log('location found:', location);
+        },
+      });
+      return null;
+    }
 
-  //   const onEachFeature = (feature, layer) => {
-  //     layer.on({
-  //       mouseover: highlightFeature,
-  //       mouseout: resetHighlight,
-  //       click: zoomToFeature,
-  //     });
-  //   };
+    const highlightFeature = (e) => {
+      let layer = e.target;
+      const { NAME } = e.target.feature.properties;
+      // setSelected({
+      //   province: `${NAME_3}, ${NAME_2}`,
+      //   count: COUNT
+      // });
+      layer.setStyle({
+        weight: 2,
+        color: '#DF1995',
+        dashArray: '',
+        fillOpacity: 1,
+      });
+      // if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      //   layer.bringToFront();
+      // }
+    };
+    const resetHighlight = (e) => {
+      // setSelected({});
+      e.target.setStyle(style(e.target.feature));
+    };
+    const zoomToFeature = (e) => {
+      console.log('TARGET FEATURE', e.target);
+      mapRef.fitBounds(e.target.getBounds());
+    };
 
-  //   const onZoomEnd = (map) => {
-  //     const currentZoom = map.getZoom();
-  //     console.log('ZOOM', currentZoom);
-  //   };
+    const onEachFeature = (feature, layer) => {
+      layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature,
+      });
+    };
 
-  //   return (
-  //     <MapContainer
-  //       className='sidebar-map'
-  //       style={{ height: '91vh', zIndex: 0, minHeight: 390, minWidth: 768 }}
-  //       center={center}
-  //       zoom={zoom}
-  //       scrollWheelZoom={false}
-  //       whenCreated={(map) => {
-  //         map.on('zoomend', function (e) {
-  //           onZoomEnd(map);
-  //         });
-  //         setMap(map);
-  //       }}
-  //       boundsOptions={bounds}
-  //       maxBounds={bounds}
-  //       minZoom={5}
-  //       zoomControl={false}
-  //     >
-  //       <TileLayer
-  //         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  //         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-  //       />
-  //       <GeoJSON data={statesdata} onEachFeature={onEachFeature} />
-  //       {/* <GeoJSON data={MaryLandCongressionalDistricts} />
-  //       <GeoJSON data={MichiganCongressionalDistricts} />
-  //       <GeoJSON data={UtahCongressionalDistricts} />
-  //       <GeoJSON data={MaryLandVotingDistricts} />
-  //       <GeoJSON data={MichiganCongressionalDistricts} />
-  //       <GeoJSON data={UtahCongressionalDistricts} /> */}
-  //       <ZoomControl position='bottomright' />
-  //     </MapContainer>
-  //   );
-  // }, [map]);
+    const onZoomEnd = (map) => {
+      const currentZoom = map.getZoom();
+    };
+
+    return (
+      <MapContainer
+        className='sidebar-map'
+        style={{ height: '91vh', zIndex: 0, minHeight: 390, minWidth: 768 }}
+        center={center}
+        zoom={zoom}
+        scrollWheelZoom={false}
+        whenCreated={(map) => {
+          mapRef = map;
+          setMap(map);
+        }}
+        boundsOptions={bounds}
+        maxBounds={bounds}
+        minZoom={5}
+        zoomControl={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <MapEvents />
+        <GeoJSON data={statesdata} onEachFeature={onEachFeature} />
+        {/* <GeoJSON data={MaryLandCongressionalDistricts} />
+        <GeoJSON data={MichiganCongressionalDistricts} />
+        <GeoJSON data={UtahCongressionalDistricts} />
+        <GeoJSON data={MaryLandVotingDistricts} />
+        <GeoJSON data={MichiganCongressionalDistricts} />
+        <GeoJSON data={UtahCongressionalDistricts} /> */}
+        <ZoomControl position='bottomright' />
+      </MapContainer>
+    );
+  }, [map]);
 
   return (
     <>
@@ -130,8 +146,7 @@ const Map = () => {
       <div style={{ position: 'relative' }}>
         {map ? <Sidebar map={map} /> : null}
         {map ? <Position map={map} /> : null}
-        {/* {displayMap} */}
-        <M />
+        {displayMap}
       </div>
     </>
   );
