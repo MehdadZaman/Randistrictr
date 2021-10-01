639aefeabaa4e9fdecdca93e1dfb4f01c55283a4
Kevin Chao <kevinchao2011@gmail.com>
2021-09-30 23:42:43 -0400

    geojson sent with proper query functioning

--- backend/src/main/java/MothBalls/Randistrictr/RedistrictingController.java
+++ backend/src/main/java/MothBalls/Randistrictr/RedistrictingController.java
@@ -1,26 +1,46 @@
 package MothBalls.Randistrictr;
 
 import MothBalls.Randistrictr.Person;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("api/redistricting")
+import java.io.FileReader;
+
+
+@RequestMapping("api")
 @RestController
 public class RedistrictingController {
-//    @GetMapping
-
-//    @PostMapping
-//    public void printPost(@RequestBody Person p) {
-//        System.out.println("p.getName()");
-//    }
-
-    @GetMapping
-    public void getAllStudents() {
-        System.out.println("YEET");
+    @GetMapping("/redistricting")
+    public JSONObject getRedistricting(@RequestParam String stateName, @RequestParam String redistrictNumber) {
+        return parseGEOJSON(stateName);
     }
 
-//    @GetMapping
-//    public void printPost() {
-//        System.out.println(o);
-//    }
+
+    //Statenames are acronyms
+    // i.e. MD == Maryland, MI = Michigan, UT = Utah
+    // take the content root when getting path
+    public JSONObject parseGEOJSON(String stateName) {
+        Object obj = new Object();
+        JSONParser parser = new JSONParser();
+        try {
+            switch (stateName) {
+                case "MD":
+                    obj = parser.parse(new FileReader("src/main/java/MothBalls/Randistrictr/constants/maryland_congressional_districts.json"));
+                    break;
+                case "MI":
+                    obj = parser.parse(new FileReader("src/main/java/MothBalls/Randistrictr/constants/michigan_congressional_districts.json"));
+                    break;
+                case "UT":
+                    obj = parser.parse(new FileReader("src/main/java/MothBalls/Randistrictr/constants/utah_congressional_districts.json"));
+                    break;
+            }
+            return (JSONObject) obj;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
